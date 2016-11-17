edaeb7e321aed87afdfbb94df879595c25e7b461
juancra264 <juancra264@hotmail.com>
2016-11-17 18:09:38 -0500

    Actualizacion de scripts ffmpeg

--- mini2.sh
+++ mini2.sh
@@ -6,15 +6,15 @@
 #		ENTRADA
 #		Board de Entrada: 			Decklink Mini Recorder	
 #		Puerto de entrada:			HDMI
-#		Tipo de senal de entrada:	720p60	(HD HDMI)
+#		Tipo de senal de entrada:	1080i59.94	(HD HDMI)
 #		 
 #		SALIDA
-#		Tipo de salida:				FILE cada 5 min
+#		Tipo de salida:				FILE cada 5 mins
 #		Tamano de cuadro:			848x480
 #		Codec video:				H.264
-#	    Deinterlace:				Deshabilitado (yadif=0:0:1)
-#		Frame rate:					5 fps
-#		Bitrate video:				250Kbps		
+#		Deinterlace:				Habilitado (yadif=0:0:1)
+#		Frame rate:					29 fps
+#		Bitrate video:				250Kbps
 #		Codec audio:				NO AUDIO
 #		Bitrate audio:				NO AUDIO
 #		Contenedor:					.mp4
@@ -31,15 +31,14 @@ ffmpeg \
  -video_input "hdmi" \
  -hide_banner \
  -f decklink \
- -i 'DeckLink Mini Recorder@15' \
+ -i 'DeckLink Mini Recorder@11' \
  -vsync 0 \
- -filter_complex "scale=848x480,pad=848:500:0:0:0x00008B,drawtext=fontfile='/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf':text='RCN TV S.A - BettySAT Analizador1 '%{localtime}'':x=5:y=486:fontcolor=yellow:fontsize=12" \
+ -filter_complex "scale=848x480,yadif=0:0:1,pad=848:500:0:0:0x00008B,drawtext=fontfile='/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf':text='RCN TV S.A - BettySAT Analizador1 '%{localtime}'':x=5:y=486:fontcolor=yellow:fontsize=12" \
  -f segment \
  -vcodec libx264 \
  -profile:v baseline \
  -level 3 \
  -pix_fmt yuv420p \
- -r 5 \
  -force_key_frames 'expr:gte(t,n_forced*2)' \
  -b:v 250k \
  -an \
